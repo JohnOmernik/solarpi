3addf69f4b729278fd0bebb68e873a33c24ce312
John Omernik <john@omernik.com>
2018-11-04 08:12:15 -0600

    Added a data archive field to the gitignore

--- test_motor.py
+++ test_motor.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import time
+import sys
 from dual_g2_hpmd_rpi import motors, MAX_SPEED
 
 #480 is Positive 100% voltage
@@ -13,22 +14,29 @@ from dual_g2_hpmd_rpi import motors, MAX_SPEED
 def main():
     motors.enable()
     motors.setSpeeds(0, 0)
-    
-    print("Setting Motor 1 to +480")
-    motors.motor1.setSpeed(480)
-    time.sleep(10)
-    print("Stopping Motor")
-    motors.motor1.setSpeed(0)
-    time.sleep(10)
-    print("seeting Motor 1 to -480")
-    motors.motor1.setSpeed(-480)
-    time.sleep(10)
-    print("Stopping Motor")
-    print("Running end to disable")
-    motors.setSpeeds(0, 0)
-    motors.disable()
-    print("Motors Disabled")
-    
+
+    inp = ""
+    while inp != "q":
+        try:
+            inp = input("f, b, s, or q: ")
+        except:
+            print("\nNo fun exiting")
+            motors.setSpeeds(0, 0)
+            motors.disable()
+            sys.exit(0)
+
+        if inp == "f": 
+            motors.motor1.setSpeed(-480)
+        elif inp == "b":
+            motors.motor1.setSpeed(480)
+        elif inp == "s":
+            motors.motor1.setSpeed(0)
+        elif inp == "q":
+            motors.setSpeeds(0, 0)
+            motors.disable()
+            sys.exit(0)
+        else:
+            print("nope")
 
 if __name__ == '__main__':
     main()
